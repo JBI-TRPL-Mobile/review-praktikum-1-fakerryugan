f5f6dcfda3c339957f73b8329e1a0c811037a41c
fakerryugan <fatkurirham55@gmail.com>
2024-10-14 11:49:06 +0700

    SELESAI

--- lib/pages/about.dart
+++ lib/pages/about.dart
@@ -3,13 +3,27 @@ import 'package:flutter/material.dart';
 class About extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(8),
-      color: Colors.blue,
-      child: Text(
-        'FATKUR ROHMAN IRHAM-36235830201',
-        style: TextStyle(color: Colors.white),
-        textAlign: TextAlign.center,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Tentang Aplikasi'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Aplikasi Buku',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            Text('Developer: FATKUR ROHMAN IRHAM'),
+            SizedBox(height: 10),
+            Text('NIM: 362358302012'),
+            SizedBox(height: 10),
+            Text('Versi: 1.0')
+          ],
+        ),
       ),
     );
   }
